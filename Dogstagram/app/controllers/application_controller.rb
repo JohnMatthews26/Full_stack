6b6d074df5619e50f94e458ac67f4132c9370185
John Matthews <johnmatthews2634@gmail.com>
2017-07-18 11:35:10 -0700

    through phase 1 of auth

--- Dogstagram/app/controllers/application_controller.rb
+++ Dogstagram/app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def login!(user)
+    session[:session_token] = user.session_token
+  end
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
 end
---
+++ Dogstagram/frontend/dogstagram.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to Dogstagram</h1>, root);
+});
