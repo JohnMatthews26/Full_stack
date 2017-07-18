d07a242aa2338aa410b7c4519b8421f5915df202
John Matthews <johnmatthews2634@gmail.com>
2017-07-18 11:45:11 -0700

    add app and root components

--- Dogstagram/frontend/dogstagram.jsx
+++ Dogstagram/frontend/dogstagram.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-
-
 
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Dogstagram</h1>, root);
