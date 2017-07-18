a4b52c1c7faef6e573d103e9daafa836dc7ef7de
John Matthews <johnmatthews2634@gmail.com>
2017-07-18 13:40:13 -0700

    add session form and container

--- Dogstagram/frontend/components/App.jsx
+++ Dogstagram/frontend/components/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
 
 const App = () => (
   <div>
-    <h1>Dogstagram</h1>
+    <header>
+      <h1>Dogstagram</h1>
+      <GreetingContainer />
+    </header>
+    <Route path="/login" component={SessionFormContainer} />
+    <Route path="/signup" component={SessionFormContainer} />
   </div>
 );
 
