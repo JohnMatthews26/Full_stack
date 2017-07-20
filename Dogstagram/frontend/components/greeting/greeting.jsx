24c8db9b659f890a30673f26ec04f655c279a4af
John Matthews <johnmatthews2634@gmail.com>
2017-07-20 11:50:40 -0700

    fix logo location

--- Dogstagram/frontend/components/greeting/greeting.jsx
+++ Dogstagram/frontend/components/greeting/greeting.jsx
@@ -7,13 +7,20 @@ const sessionLinks = () => (
       <Link to="/login" className="login_signup_links">Login/Demo</Link>
       <Link to="/signup" className="login_signup_links">Sign up</Link>
     </section>
+    <section className="logo_section">
+      <h1 className="logo"><img className="logo"src="http://res.cloudinary.com/roscoe/image/upload/c_scale,w_150/v1500572600/logo_bxburg.jpg" alt="logo"></img></h1>
+    </section>
   </nav>
 );
 
 const personalGreeting = (currentUser, logout) => (
 	<hgroup className="header-group">
+    <section className="logo_section">
+      <h1 className="logo"><img className="logo"src="http://res.cloudinary.com/roscoe/image/upload/c_scale,w_70/v1500572600/logo_bxburg.jpg" alt="logo"></img></h1>
+    </section>
     <h2 className="header-name">Hi, {currentUser.username}!</h2>
     <button className="header-button" onClick={logout}>Log Out</button>
+
 	</hgroup>
 );
 
