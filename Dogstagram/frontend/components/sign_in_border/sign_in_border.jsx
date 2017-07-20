333ba368c5397851f721709b959ed8f3251fca4d
John Matthews <johnmatthews2634@gmail.com>
2017-07-20 10:07:02 -0700

    remove dog photo border and add color border on login/signup page

--- Dogstagram/frontend/components/sign_in_border/sign_in_border.jsx
+++ Dogstagram/frontend/components/sign_in_border/sign_in_border.jsx
@@ -3,31 +3,12 @@ import { Link } from 'react-router-dom';
 
 const loggedOutBorder = () => (
   <nav className="log-out-border-div">
-    <section className="logged-out-border">
-      <ul className="borderimages">
-        <li className="borderimage">
-          <img src="http://res.cloudinary.com/roscoe/image/upload/v1500503776/IMG_0370_woasjv.jpg"></img>
-        </li>
-        <li className="borderimage">
-          <img src="http://res.cloudinary.com/roscoe/image/upload/v1500503776/IMG_0354_p3hunu.jpg"></img>
-        </li>
-        <li className="borderimage">
-          <img src="http://res.cloudinary.com/roscoe/image/upload/v1500503775/IMG_0368_iwy0b9.jpg"></img>
-        </li>
-        <li className="borderimage">
-          <img src="http://res.cloudinary.com/roscoe/image/upload/v1500503776/IMG_0352_mnuoym.jpg"></img>
-        </li>
-        <li className="borderimage">
-          <img src="http://res.cloudinary.com/roscoe/image/upload/v1500503774/IMG_0377_ijtmvv.jpg"></img>
-        </li>
-        <li className="borderimage">
-          <img src="http://res.cloudinary.com/roscoe/image/upload/v1500503773/IMG_0375_vzbmis.jpg"></img>
-        </li>
-        <li className="borderimage">
-          <img src="http://res.cloudinary.com/roscoe/image/upload/v1500503773/IMG_0390_zvtjua.jpg"></img>
-        </li>
+    <section className="picture_of_app">
+      
+    </section>
 
-      </ul>
+    <section className="bottom_border_logged_out">
+
     </section>
   </nav>
 );
