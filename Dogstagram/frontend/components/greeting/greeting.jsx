f8a724c8a8150f0083892790029ffae6f7702778
John Matthews <johnmatthews2634@gmail.com>
2017-07-21 16:47:31 -0700

    add my profile button

--- Dogstagram/frontend/components/greeting/greeting.jsx
+++ Dogstagram/frontend/components/greeting/greeting.jsx
@@ -20,6 +20,7 @@ const personalGreeting = (currentUser, logout) => (
     </section>
     <h2 className="header-name">Hi, {currentUser.username}!</h2>
     <button className="header-button" onClick={logout}>Log Out</button>
+    <Link to={`/users/${currentUser.id}`} className="user_profile_link">My Profile</Link>
 
 	</hgroup>
 );
