c8759aea7f8538983d4c848460e0382a92e6fbee
John Matthews <johnmatthews2634@gmail.com>
2017-07-25 11:36:57 -0700

    add photo show modal

--- Dogstagram/frontend/components/photo/photo_index_item.jsx
+++ Dogstagram/frontend/components/photo/photo_index_item.jsx
@@ -1,14 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import Modal from 'react-modal';
 
-const PhotoIndexItem = ({ photo }) => (
-  <li className="photo-index-item">
-    <Link to={`/photos/${photo.id}`}>
+class PhotoIndexItem extends Component {
+  constructor({photo}, props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false,
+      url: photo.url,
+      id: photo.id
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
 
-      <img src={photo.url} alt={photo.id} className="photo-index-item" />
+  closeModal() {
+    this.setState({modalIsOpen: false});
+    window.location.reload();
+  }
 
-    </Link>
-  </li>
-);
+  render(){
+    return (
+      <div>
+        <li className="photo-index-item">
+            <button onClick={this.openModal} >
+              <img src={this.state.url} alt={this.state.id} className="photo-index-item" />
+                <Modal
+                isOpen={this.state.modalIsOpen}
+                onRequestClose={this.closeModal}
+                shouldCloseOnOverlayClick={true}
+                contentLabel="PhotoShowModal"
+                className="photo-show-modal"
+                >
+                  <div className="image-show-modal-box">
+                    <img src={this.state.url} alt={this.state.id} className="photo-show" />
+                  </div>
+                </Modal>
+            </button>
+
+        </li>
+      </div>
+    );
+  }
+}
+// const PhotoIndexItem = ({ photo }) => (
+//   <li className="photo-index-item">
+//       <button onClick={modalIsOpen(true)} >
+//         <img src={photo.url} alt={photo.id} className="photo-index-item" />
+//           <Modal
+//           isOpen={modalIsOpen}
+//           onRequestClose={closeModal}
+//           shouldCloseOnOverlayClick={false}
+//           contentLabel="PhotoShowModal"
+//           className="photo-show-modal"
+//           >
+//             <div className="image-show-modal-box">
+//               <img src={photo.url} alt={photo.id} className="photo-index-item" />
+//             </div>
+//           </Modal>
+//       </button>
+//
+//
+//
+//
+//   </li>
+// );
 
 export default PhotoIndexItem;
