15d20906ca4364a52af5ad67305daa096fa79a6a
John Matthews <johnmatthews2634@gmail.com>
2017-11-01 12:59:24 -0700

    :

--- Dogstagram/frontend/components/App.jsx
+++ Dogstagram/frontend/components/App.jsx
@@ -20,6 +20,7 @@ import PhotoFeedContainer from './photo_feed/photo_feed_container';
 
 const App = () => (
   <div className="mainDiv">
+    <div className='main-page-div'>
     <section className="login-signup_container">
       <header>
         <GreetingContainer />
@@ -29,6 +30,12 @@ const App = () => (
         <AuthRoute path="/signup" component={SessionFormContainer} />
       </Switch>
     </section>
+    <div className='splash-image-div'>
+      <img className='splash-image' src='http://res.cloudinary.com/roscoe/image/upload/v1500570035/iphone_image_qlxqp2.jpg'>
+
+      </img>
+    </div>
+    </div>
       <SignInBorderContainer />
 
     <Switch>
