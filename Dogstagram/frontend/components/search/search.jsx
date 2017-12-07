1da2fc818664fcabc48d67ddb891f4ff5bc9cb5c
John Matthews <johnmatthews2634@gmail.com>
2017-12-07 13:18:32 -0800

    search bar successfully showing users and links to profile

--- Dogstagram/frontend/components/search/search.jsx
+++ Dogstagram/frontend/components/search/search.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import {withRouter} from 'react-router';
+import { Link } from 'react-router-dom';
+
 
 class Search extends Component {
 
@@ -15,6 +17,61 @@ class Search extends Component {
     this.props.requestAllUsers();
   }
 
+
+//   usersList(){
+//     let len = this.state.username.length - 1;
+//     let entry = this.state.username.slice(0, len).toLowerCase();
+//     if (len > 0){
+//       let validUsers = [];
+//       let usersArray = Object.entries(this.props.users.entities);
+//       usersArray.forEach(function(value) {
+//         let usernamePartial = value[1].username.slice(0, len).toLowerCase();
+//         // console.log(usernamePartial);
+//         // console.log(entry);
+//         // console.log(len);
+//         if (entry == usernamePartial) {
+//           validUsers.push(value[1].username);
+//         }
+//       });
+//       console.log(validUsers);
+//       return validUsers;
+//     }
+//   }
+//
+  // update(field) {
+  //   this.usersList();
+  //   return e => this.setState({
+  //     [field]: e.currentTarget.value
+  //   });
+  // }
+//
+//   dropdown(usersArray){
+//
+//   }
+//
+//
+//
+//
+//   render()  {
+//     const users = this.props.users;
+//     return (
+//       <section className="users-list">
+//         <form>
+//           <input type="text"
+//             value={this.state.username}
+//             onChange={this.update('username')}
+//             className="search-input"
+//             placeholder="Search Users"
+//           />
+//         </form>
+//       </section>
+//     );
+//   }
+//
+// }
+//
+// export default withRouter(Search);
+
   usersList(){
     let len = this.state.username.length - 1;
     let entry = this.state.username.slice(0, len).toLowerCase();
@@ -23,25 +80,29 @@ class Search extends Component {
       let usersArray = Object.entries(this.props.users.entities);
       usersArray.forEach(function(value) {
         let usernamePartial = value[1].username.slice(0, len).toLowerCase();
+        let userId = value[0];
         // console.log(usernamePartial);
-        // console.log(entry);
+        // console.log(value);
         // console.log(len);
-
         if (entry == usernamePartial) {
-          validUsers.push(value[1].username);
+          validUsers.push([userId, value[1].username]);
         }
       });
       console.log(validUsers);
-      return validUsers;
+      return (
+        <ul className="users-list">
+        {validUsers.map(el => <Link to={`/users/${el[0]}`}><li key="${el[1]}" className="user-link">{el[1]}</li></Link>)}
+        </ul>
+      );
     }
   }
 
   update(field) {
-    this.usersList();
     return e => this.setState({
       [field]: e.currentTarget.value
     });
   }
+
 
 
 
@@ -58,6 +119,7 @@ class Search extends Component {
             placeholder="Search Users"
           />
         </form>
+        {this.usersList()}
       </section>
     );
   }
